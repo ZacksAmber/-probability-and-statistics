b3805f79842aa8f5ca538102c828cd8615b964ea
Zacks <Zacks.Shen@gmail.com>
2022-02-28 00:14:32 -0800

    Complete Random Variables

--- utils/src/_stats/norm/norm_prop.py
+++ utils/src/_stats/norm/norm_prop.py
@@ -1,5 +1,6 @@
 from scipy.stats import norm
-from utils.zscore import zscore
+from utils.src._stats.zscore import zscore
+
 
 def norm_prop(dp, mu, sd, area='below'):
     """norm_prop returns the proportion of a specific area based on dp.
@@ -27,8 +28,10 @@ def norm_prop(dp, mu, sd, area='below'):
         p = 1 - norm.cdf(zscore(dp, mu, sd))
     # Proportion between dp_low and dp_high
     else:
+        if isinstance(dp, (int, float, dict, str)):
+            raise TypeError("dp must be an array like type!")
         if len(dp) != 2:
-            raise ValueError("dp must be 2 for calculating proportion between two data points!")
+            raise ValueError("dp must contains two data points!")
         dp_low, dp_high = min(dp), max(dp)
         p = norm.cdf(zscore(dp_high, mu, sd)) - norm.cdf(zscore(dp_low, mu, sd))
 
---
+++ utils/src/stats.py
@@ -0,0 +1,3 @@
+from ._stats.zscore import zscore
+from ._stats.norm.norm_prop import norm_prop
+from ._stats.norm.norm_rv import norm_rv
